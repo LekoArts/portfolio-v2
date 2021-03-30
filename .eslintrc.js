1b9c3bbff5f987ca76c4a250ef2b8c5c3c44100f
LekoArts <lekoarts@gmail.com>
2021-03-30 16:58:37 +0200

    package updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,6 @@
 module.exports = {
   parser: `@typescript-eslint/parser`, // Specifies the ESLint parser
-  extends: [
-    `airbnb`,
-    `plugin:@typescript-eslint/recommended`,
-    `plugin:import/typescript`,
-    `plugin:prettier/recommended`,
-    `prettier/@typescript-eslint`,
-  ],
+  extends: [`airbnb`, `plugin:prettier/recommended`],
   plugins: [`@typescript-eslint`, `prettier`, `react-hooks`],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
