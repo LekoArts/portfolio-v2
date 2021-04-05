8e06a837d770cb0e613a11a8f1b82b0b6bcc2f3a
LekoArts <lekoarts@gmail.com>
2021-04-05 18:23:56 +0200

    wip garden footer

--- www/src/templates/garden.tsx
+++ www/src/templates/garden.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { PageProps, graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
-import { Container, Text, Divider, Flex, Tag, TagLabel, Grid } from "@chakra-ui/react"
+import { Container, Text, Divider, Flex, Tag, TagLabel, Grid, Box, Link as ExternalLink } from "@chakra-ui/react"
 import { Layout } from "../components/blocks/layout"
 import { Link } from "../components/link"
 import { SkipNavContent } from "../components/a11y/skip-nav"
@@ -23,10 +23,21 @@ type DataProps = {
     tags: string[]
     timeToRead: number
     excerpt: string
+    parent: {
+      parent: {
+        relativePath: string
+      }
+    }
   }
 }
 
-const Garden: React.FC<PageProps<DataProps>> = ({ data: { garden } }) => (
+const getTwitterShareLink = (path: string): string =>
+  `https://www.twitter.com/search?q=${encodeURIComponent(`https://www.lekoarts.de${path}`)}`
+
+const getGitHubEditLink = (path: string): string =>
+  `https://github.com/LekoArts/portfolio-v2/edit/master/www/content/garden/${path}`
+
+const Garden: React.FC<PageProps<DataProps>> = ({ data: { garden }, location: { pathname } }) => (
   <Layout>
     <SEO title={garden.title} description={garden.excerpt} image="/social/digital-garden.png">
       <meta name="twitter:label1" value="Time To Read" />
@@ -42,13 +53,16 @@ const Garden: React.FC<PageProps<DataProps>> = ({ data: { garden } }) => (
         <Spacer size={6} axis="vertical" />
         <Divider />
         <Spacer size={4} axis="vertical" />
-        <Grid gridGap={2} gridTemplateColumns="1fr 200px">
+        {garden.slug}
+        <Grid gridGap={2} gridTemplateColumns={[`1fr`, null, `1fr auto`]}>
           <Text>
             Created {garden.date} – Last Updated {garden.lastUpdated}
           </Text>
-          <Flex>
+          <Flex flexWrap="wrap" justifyContent={[`flex-start`, null, `flex-end`]}>
             {garden.tags.map((tag) => (
-              <span key={tag}>{tag}</span>
+              <Box as="span" ml={2} _first={{ ml: 0 }} key={tag}>
+                {tag}
+              </Box>
             ))}
           </Flex>
           <Tag colorScheme="green" justifySelf="flex-start">
@@ -61,6 +75,10 @@ const Garden: React.FC<PageProps<DataProps>> = ({ data: { garden } }) => (
         <Prose>
           <MDXRenderer>{garden.body}</MDXRenderer>
         </Prose>
+        <div>
+          <ExternalLink href={getGitHubEditLink(garden.parent.parent.relativePath)}>Edit on GitHub</ExternalLink> -{` `}
+          <ExternalLink href={getTwitterShareLink(pathname)}>Discuss on Twitter</ExternalLink>
+        </div>
       </SkipNavContent>
     </Container>
   </Layout>
@@ -82,6 +100,17 @@ export const query = graphql`
       tags
       timeToRead
       excerpt
+      ... on MdxGarden {
+        parent {
+          ... on Mdx {
+            parent {
+              ... on File {
+                relativePath
+              }
+            }
+          }
+        }
+      }
     }
   }
 `
