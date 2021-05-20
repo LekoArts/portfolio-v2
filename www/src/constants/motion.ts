62f1568a1eaf5212e150376844d249ab1ac8ffa5
LekoArts <lekoarts@gmail.com>
2021-05-20 16:57:20 +0200

    rename ui to design, smaller fixes

--- www/src/constants/motion.ts
+++ www/src/constants/motion.ts
@@ -1,7 +1,7 @@
 export const transforms = {
   beforeHover: {
     transform: `translate3d(0, 0, 0)`,
-    transition: `transform 0.4s cubic-bezier(0.34, 1.56, 0.64, 1)`,
+    transition: `transform 0.4s cubic-bezier(0.34, 1.56, 0.64, 1), box-shadow 0.4s cubic-bezier(0.34, 1.56, 0.64, 1)`,
   },
   onHover: {
     transform: `translate3d(0, -8px, 0)`,
