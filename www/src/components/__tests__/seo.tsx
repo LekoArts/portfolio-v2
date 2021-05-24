774e91a13745b70f687bbc74b761f1034ba559c1
LekoArts <lekoarts@gmail.com>
2021-05-24 16:33:20 +0200

    finish seo test, add github action for testing

--- www/src/components/__tests__/seo.tsx
+++ www/src/components/__tests__/seo.tsx
@@ -40,8 +40,38 @@ describe(`SEO component`, () => {
     const meta = mockUseStaticQuery.site.siteMetadata
 
     expect(title).toBe(meta.siteTitleDefault)
+    expect(metaTags.find((tag) => tag.property === `og:description`).content).toBe(meta.siteDescription)
     expect(htmlAttributes.lang).toBe(`en-US`)
     expect(linkTags.find((tag) => tag.rel === `canonical`).href).toBe(meta.siteUrl)
     expect(metaTags.find((tag) => tag.name === `robots`)).toBe(undefined)
+    expect(metaTags.find((tag) => tag.property === `og:image`).content).toBe(`${meta.siteUrl}${meta.siteImage}`)
+  })
+  it(`should accept all common props`, () => {
+    render(
+      <SEO
+        title="Custom Title"
+        image="/path/to/image.png"
+        description="Custom Description"
+        pathname="/custom-path"
+        breadcrumbListItems={[{ name: `Hermione`, url: `/granger` }]}
+      />
+    )
+    const { scriptTags, metaTags, title } = Helmet.peek()
+    const meta = mockUseStaticQuery.site.siteMetadata
+    const breadcrumbJSON = JSON.parse(scriptTags[0].innerHTML)
+
+    expect(title).toBe(`Custom Title | Harry Potter`)
+    expect(metaTags.find((tag) => tag.property === `og:description`).content).toBe(`Custom Description`)
+    expect(metaTags.find((tag) => tag.property === `og:url`).content).toBe(`${meta.siteUrl}/custom-path`)
+    expect(metaTags.find((tag) => tag.property === `og:image`).content).toBe(`${meta.siteUrl}/path/to/image.png`)
+    expect(breadcrumbJSON.name).toBe(`Breadcrumbs`)
+    expect(breadcrumbJSON.itemListElement.find((el) => el.position === 1).item.name).toBe(`Homepage`)
+    expect(breadcrumbJSON.itemListElement.find((el) => el.position === 2).item.name).toBe(`Hermione`)
+  })
+  it(`should hide with noIndex`, () => {
+    render(<SEO noIndex />)
+    const { metaTags } = Helmet.peek()
+
+    expect(metaTags.find((tag) => tag.name === `robots`).content).toBe(`noindex, nofollow`)
   })
 })
