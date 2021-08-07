1dc049b5d600c6a968696242746060b3ab4f4b8d
Lennart <lekoarts@gmail.com>
2021-08-07 14:31:26 +0200

    feat: Add Collapsible component (#25)

---
+++ www/src/components/mdx/collapsible.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Box, useColorModeValue } from "@chakra-ui/react"
+
+export const Collapsible: React.FC<{ summary: React.ReactNode }> = ({ summary, children }) => {
+  const bgColor = useColorModeValue(`blue.50`, `blueGray.800`)
+
+  return (
+    <Box as="details" bgColor={bgColor} px={[4, null, 6]} py={4} borderRadius="lg">
+      <Box as="summary" display="list-item" textStyle="prominent" sx={{ ">:first-child": { display: `inline` } }}>
+        {summary}
+      </Box>
+      {children}
+    </Box>
+  )
+}
