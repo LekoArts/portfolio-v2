ca60e860c55ec0a2d7bff62b6b65a241ceea427a
LekoArts <lekoarts@gmail.com>
2022-05-08 14:18:03 +0100

    font loading

--- www/src/components/blocks/fonts.tsx
+++ www/src/components/blocks/fonts.tsx
@@ -1,5 +1,9 @@
 import * as React from "react"
 import { Global } from "@emotion/react"
+// @ts-ignore
+import interVariableWoff2 from "../../assets/fonts/Inter-roman.var.woff2"
+// @ts-ignore
+import crimsonProVariableWoff2 from "../../assets/fonts/Crimson-Pro.var.woff2"
 
 export const Fonts = () => (
   <Global
@@ -12,7 +16,7 @@ export const Fonts = () => (
       font-display: swap;
       font-style: normal;
       font-named-instance: 'Regular';
-      src: url(/fonts/Inter-roman.var.woff2?v=3.19) format("woff2");
+      src: url(${interVariableWoff2}) format("woff2");
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
     }
 
@@ -23,7 +27,7 @@ export const Fonts = () => (
       font-style: normal;
       font-weight: 600 800;
       font-display: swap;
-      src: url(/fonts/Crimson-Pro.var.woff2?v=1) format('woff2');
+      src: url(${crimsonProVariableWoff2}) format('woff2');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
     }
   `}
