3030f3ea4eff29ca568b89f96af1a7e62ca0a88c
LekoArts <lekoarts@gmail.com>
2022-05-27 09:39:50 +0200

    fix font loading

--- www/gatsby-ssr.tsx
+++ www/gatsby-ssr.tsx
@@ -1,6 +1,10 @@
 import * as React from "react"
 import type { GatsbySSR } from "gatsby"
 import { site } from "./src/constants/meta"
+// @ts-ignore
+import interVariableWoff2 from "./src/assets/fonts/Inter-roman.var.woff2"
+// @ts-ignore
+import crimsonProVariableWoff2 from "./src/assets/fonts/Crimson-Pro.var.woff2"
 
 const PLAUSIBLE_DOMAIN = `plausible.io`
 const SCRIPT_URI = `/js/plausible.js`
@@ -13,9 +17,23 @@ export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHeadComponents }) =
     }
 
     return setHeadComponents([
-      <link rel="preload" href="/icons.svg" as="image/svg+xml" />,
-      <link rel="preload" href="/fonts/Inter-roman.var.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />,
-      <link rel="preload" href="/fonts/Crimson-Pro.var.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />,
+      <link rel="preload" href="/icons.svg" as="image" type="image/svg+xml" key="svgIcons" />,
+      <link
+        rel="preload"
+        href={interVariableWoff2}
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+        key="interFont"
+      />,
+      <link
+        rel="preload"
+        href={crimsonProVariableWoff2}
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+        key="crimsonFont"
+      />,
       <link key="gatsby-plugin-plausible-preconnect" rel="preconnect" href={`https://${PLAUSIBLE_DOMAIN}`} />,
       <script key="gatsby-plugin-plausible-script" defer {...scriptProps} />,
       // See: https://plausible.io/docs/custom-event-goals#1-trigger-custom-events-with-javascript-on-your-site
