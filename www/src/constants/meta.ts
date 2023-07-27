60a31f2cbb3bfd9797bf66aa0b8bde3cc025cdb6
LekoArts <lekoarts@gmail.com>
2023-07-27 09:24:26 +0200

    rename default og image

--- www/src/constants/meta.ts
+++ www/src/constants/meta.ts
@@ -4,6 +4,6 @@ export const site = {
   url: `https://www.lekoarts.de`, // Without a trailing slash!
   dataDomain: `lekoarts.de`,
   description: `Lennart is a software engineer and passionate about working on open source products & building communities around them. He currently works at Gatsby on the open source project.`,
-  image: `/social/default-og-image.png?v=1`,
+  defaultOgImage: `/social/default-og-image.png?v=1`,
   twitter: `@lekoarts_de`,
 }
