14833a1b30fe4010459a3924fbc75ae7103c4c1a
LekoArts <lekoarts@gmail.com>
2023-08-28 16:10:27 +0200

    chore: Update deps

--- vitest/setup.ts
+++ vitest/setup.ts
@@ -1,6 +1,6 @@
 import { vi } from "vitest"
 import React from "react"
-import "@testing-library/jest-dom"
+import "@testing-library/jest-dom/vitest"
 
 vi.mock(`gatsby`, async () => {
   const gatsby = await vi.importActual<typeof import("gatsby")>(`gatsby`)
