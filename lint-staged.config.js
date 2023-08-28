af7e7869c7453fa41872596315dbf4572c4fda88
LekoArts <lekoarts@gmail.com>
2023-08-28 15:57:29 +0200

    chore: Update linting

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "*.{js,ts,tsx,mdx}": [`eslint --ignore-path .gitignore --ignore-path .prettierignore --fix`],
+  "*.{js,ts,tsx,mdx,mjs}": [`eslint --ignore-path .gitignore --ignore-path .prettierignore --fix`],
   "*.{md,json,yaml}": [`prettier "**/*.{md,json,yaml}" --write`],
 }
